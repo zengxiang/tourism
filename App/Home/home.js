38f18b8d4bad2493b6db5722348773b76f19f6e2
447224251@qq.com <447224251@qq.com>
2016-09-06 15:14:48 +0800

    add

--- App/Home/home.js
+++ App/Home/home.js
@@ -3,14 +3,27 @@
  */
 
 'use strict';
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 import {
     Text,
-    View
+    View,
+    Image,
+    TextInput,
+    ListView
 } from 'react-native';
-import Button from 'react-native-button';
 
+import Button from 'react-native-button';
+import {GetScreenWidth,GetListData} from './../PublicAction'
 import {Actions} from 'react-native-router-flux';
+
+// import RefreshableListView from 'react-native-refreshable-listview';
+var RefreshableListView = require('react-native-refreshable-listview')
+
+
+// import StoreWatchMixin from './StoreWatchMixin';
+// import ArticleView from './ArticleView';
+
+
 
 export default class Home extends Component {
     constructor(props) {
@@ -23,10 +36,22 @@ export default class Home extends Component {
     }
 
     render() {
-
-        let pushNext = ()=>alert('下一个');
         return (
-            <View style={{margin: 64}}>
+            <View>
+                <View style={styles.navBg}>
+                    <View style={{height: 20}}>
+                    </View>
+                    <View style={styles.nav}>
+                        <Image style={styles.logo} source={require('./../img/logo.png')}/>
+                        <Image style={styles.searchBg} source={require('./../img/search_bg.png')}>
+                            <TextInput
+                                style={styles.searchInput}
+                                onChangeText={(text) => this.setState({text})}
+                                value={this.state.text}
+                            />
+                        </Image>
+                    </View>
+                </View>
                 <Button onPress={Actions.tab2}>跳到消息页面</Button>
                 <Button onPress={Actions.HomeDetail}>push Detail</Button>
                 <Button
@@ -44,6 +69,30 @@ export default class Home extends Component {
 }
 
 const styles = {
+    navBg: {
+        height: 64,
+        backgroundColor: '#333333',
+    },
+    nav: {
+        height: 44,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    logo: {
+        height: 44,
+        resizeMode: Image.resizeMode.contain,
+    },
+    searchBg: {
+        width: GetScreenWidth() - 44 - 70,
+        height: 30,
+        resizeMode: Image.resizeMode.contain,
+
+    },
+    searchInput: {
+        borderColor: 'white',
+        height:30 ,
+        paddingHorizontal: 10,
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
